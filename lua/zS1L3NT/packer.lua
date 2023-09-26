22ca11cd6c7de42524f9ac482135cc86a0bb538e
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-27 01:31:49 +0800

    ✨ code action menu popup

--- lua/zS1L3NT/packer.lua
+++ lua/zS1L3NT/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
             { 'neovim/nvim-lspconfig' },
             { 'williamboman/mason.nvim' },
             { 'williamboman/mason-lspconfig.nvim' },
+            { 'weilbith/nvim-code-action-menu', cmd = 'CodeActionMenu' },
 
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' },
