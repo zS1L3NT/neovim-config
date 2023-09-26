9140c20abe0b49eee819a552d054eff10d1ddc0f
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-26 16:32:28 +0800

    ✨ reconfigure telescope keybinds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,21 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+local status, telescope = pcall(require, "telescope")
+if (not status) then return end
+
+local actions = require('telescope.actions')
+local builtin = require("telescope.builtin")
+
+telescope.setup({
+    defaults = {
+        mappings = {
+            n = {
+                ["q"] = actions.close
+            },
+        },
+    },
+})
+
+vim.keymap.set('n', ';f', function() builtin.find_files({ no_ignore = false, hidden = true }) end)
+vim.keymap.set('n', ';r', builtin.live_grep)
+vim.keymap.set('n', ';e', builtin.diagnostics)
+vim.keymap.set('n', ';;', builtin.resume)
+
