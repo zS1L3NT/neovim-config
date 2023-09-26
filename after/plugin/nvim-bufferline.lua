0f1e621a2de952614430a45e0c27d826b2cf9a54
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-26 23:11:25 +0800

    ✨ configure bufferline settings

--- after/plugin/nvim-bufferline.lua
+++ after/plugin/nvim-bufferline.lua
@@ -1,8 +1,27 @@
 local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
-bufferline.setup({})
+bufferline.setup({
+    options = {
+        truncate_names = false,
+        close_command = ":bd %d|:bp",
+        middle_click_command = ":bd %d|:bp",
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function (count, level)
+          local icon = level:match("error") and "? " or "? "
+          return " " .. icon .. count
+        end,
+        offsets = {{
+            filetype = "NvimTree",
+            text = "Files",
+            text_align = "center",
+            separator = true,
+        }},
+        separator_style = "slant",
+    }
+})
 
+vim.opt.termguicolors = true
+vim.keymap.set("n", "<S-W>", ":bd %d|:bp<CR>")
 vim.keymap.set("n", "<Tab>", vim.cmd.BufferLineCycleNext)
-vim.keymap.set("n", "<S-Tab>", vim.cmd.BufferLineCyclePrev)
-
+vim.keymap.set("n", "<S-Tab>", vim.cmd.BufferLineCyclePreo)
