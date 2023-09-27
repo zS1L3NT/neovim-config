9efd640e660362ad47f6ae79de86544528dae8f0
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-28 02:44:09 +0800

    ✨ better indent blanklines character

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,9 +1,6 @@
 local status, indent_blankline = pcall(require, "indent_blankline")
 if (not status) then return end
 
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-
 indent_blankline.setup({
-    space_char_blankline = " ",
+    char = "▏"
 })
